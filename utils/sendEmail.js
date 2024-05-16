e37dd4a905cb8eb5b4d6fae0c09d86525188d85c
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-05-16 11:45:50 +0200

    feat: change email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -33,12 +33,10 @@ const sendEmail = async (email, code, res) => {
           href="https://fonts.gstatic.com"
           crossorigin
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@1,6..12,300&family=Poppins:ital,wght@0,100;0,300;1,300&family=Roboto:wght@100&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
       </head>
-      <div style="font-family: 'Poppins', sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5; border-radius: 15px; box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);">
+      <div style="font-family: 'Quicksand', 'Poppins', sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5; border-radius: 15px; box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);">
       <h2 style="color: #007BFF; text-align: center; font-size: 25px; margin-bottom: 10px;">🌟 Welcome to Place Pulse! 🌟</h2>
       <p style="color: #555; text-align: center; font-size: 16px;">Congratulations! You are now officially a part of the Place Pulse family.</p>
       <div style="text-align: center; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);">
