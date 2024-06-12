12c1f3bea2a2b546a5bb6f0e77ed8e0727c0c684
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-06-12 23:54:14 +0200

    feat: init accommodation routes

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const client = require("../database/connection");
 
-const authMiddleWare = async (req, res, next) => {
+exports.authMiddleWare = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
 
@@ -30,4 +30,4 @@ const authMiddleWare = async (req, res, next) => {
   }
 };
 
-module.exports = authMiddleWare;
+module.exports = exports;
---
+++ routes/accommodations.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { registerAccommodation } = require("../controllers/accommodations.controllers");
+const authMiddleWare = require("../middlewares/auth.middleware");
+const router = express.Router();
+
+router.post("/create", registerAccommodation)
+
+module.exports = router;
