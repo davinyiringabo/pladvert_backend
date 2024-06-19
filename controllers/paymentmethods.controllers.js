fd5c8bd026a5048a3a83599d7c2cc75962d7e2ce
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-06-19 18:17:38 +0200

    [add]: add payment methods logic

--- controllers/paymentmethods.controllers.js
+++ controllers/paymentmethods.controllers.js
@@ -17,15 +17,26 @@ const sendVerificationEmail = require("../utils/emails/verification.js");
 exports.registerPaymentMethod = async (req, res) => {
   const { number, name, type, cvc, expdate } = req.body;
   if (!type || !number) {
-    res.status(400).send({ message: "Fill All Fields (number, type)", status: 400 });
+    res
+      .status(400)
+      .send({ message: "Fill All Fields (number, type)", status: 400 });
     return;
   }
 
   try {
     const methodId = uuidv4();
-    const query = "INSERT INTO paymentmethods (id,name,number,type,cvc,expdate,user_id) VALUES ($1,$2,$3,$4,$5,$6,$7)";
-    const data = await client.query(query, [methodId, name, number, type, cvc, expdate, req.user.id]);
-    
+    const query =
+      "INSERT INTO paymentmethods (id,name,number,type,cvc,expdate,user_id) VALUES ($1,$2,$3,$4,$5,$6,$7)";
+    const data = await client.query(query, [
+      methodId,
+      name,
+      number,
+      type,
+      cvc,
+      expdate,
+      req.user.id,
+    ]);
+
     res.status(200).send({
       message: "Successfully Added Payment Method",
       data: data.rows[0],
@@ -38,40 +49,46 @@ exports.registerPaymentMethod = async (req, res) => {
 };
 
 exports.getMyMethods = async (req, res) => {
-    try {
-      const methods = await client.query("SELECT * FROM paymentmethods WHERE user_id = $1", [req.user.id]);
-      res.status(200).json({
-        message: "Payment methods fetched successfully",
-        status: 200,
-        data: methods.rows,
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({
-        message: "Internal server error",
-        status: 500,
-        data: err,
-      });
-    }
-  };
+  try {
+    const methods = await client.query(
+      "SELECT * FROM paymentmethods WHERE user_id = $1",
+      [req.user.id],
+    );
+    res.status(200).json({
+      message: "Payment methods fetched successfully",
+      status: 200,
+      data: methods.rows,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal server error",
+      status: 500,
+      data: err,
+    });
+  }
+};
 
 exports.removePaymentMethod = async (req, res) => {
-    const id = req.params.id;
-    try {
-      const methods = await client.query("DELETE FROM paymentmethods WHERE id = $1 AND user_id= $2", [id,req.user.id]);
-      res.status(200).json({
-        message: "Payment methods fetched successfully",
-        status: 200,
-        data: methods.rows,
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({
-        message: "Internal server error",
-        status: 500,
-        data: err,
-      });
-    }
-  };
+  const id = req.params.id;
+  try {
+    const methods = await client.query(
+      "DELETE FROM paymentmethods WHERE id = $1 AND user_id= $2",
+      [id, req.user.id],
+    );
+    res.status(200).json({
+      message: "Payment methods fetched successfully",
+      status: 200,
+      data: methods.rows,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal server error",
+      status: 500,
+      data: err,
+    });
+  }
+};
 
 module.exports = exports;
--- routes/paymentmethods.routes.js
+++ routes/paymentmethods.routes.js
@@ -5,11 +5,15 @@ const {
   deleteAccommodationById,
 } = require("../controllers/accommodations.controllers");
 const router = express.Router();
-const {authMiddleWare} = require("../middlewares/auth.middleware");
-const { registerPaymentMethod, getMyMethods, removePaymentMethod } = require("../controllers/paymentmethods.controllers");
+const { authMiddleWare } = require("../middlewares/auth.middleware");
+const {
+  registerPaymentMethod,
+  getMyMethods,
+  removePaymentMethod,
+} = require("../controllers/paymentmethods.controllers");
 
 router.post("/create", authMiddleWare, registerPaymentMethod);
 router.get("/getMine", authMiddleWare, getMyMethods);
-router.delete("/delete/:id", authMiddleWare,removePaymentMethod);
+router.delete("/delete/:id", authMiddleWare, removePaymentMethod);
 
 module.exports = router;
