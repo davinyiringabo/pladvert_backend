b31b92e16254c298ead3d663c0166ec69417e158
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-08-03 15:20:44 +0200

    fix: change url

--- database/connection.js
+++ database/connection.js
@@ -2,8 +2,8 @@ const pg = require("pg");
 const client = new pg.Pool({
   connectionString:
     "postgres://default:fe1v6bzPxVnX@ep-curly-snow-a4vhdch7-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
-    ssl: {
-      rejectUnauthorized: false,
-    },
-},);
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 module.exports = client;
